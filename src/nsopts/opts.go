b62ccd0754d0cf89a085dada7e2323258b95bf1c
mortawe <mortawe@gmail.com>
2020-12-21 17:09:53 +0300

    rm netsetgo path from flags

--- src/nsopts/opts.go
+++ src/nsopts/opts.go
@@ -15,7 +15,8 @@ func NewOpts() Opts {
 	o := Opts{}
 	flag.StringVar(&o.hostname, "hostname", "go-containerized", "hostname inside container")
 	flag.StringVar(&o.rootfs, "rootfs", "/tmp/go-containerized/rootfs", "path to the root filesystem to use")
-	flag.StringVar(&o.netsetgo, "netsetgo", "/usr/local/bin/netsetgo", "path to the netsetgo binary")
+	//flag.StringVar(&o.netsetgo, "netsetgo", "/usr/local/bin/netsetgo", "path to the netsetgo binary")
+	o.netsetgo = "/usr/local/bin/netsetgo"
 	flag.Parse()
 	return o
 }
